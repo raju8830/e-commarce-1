b8744f0033fdece97fd04a9ff15e3f3eac6f4592
raju8830 <44889262+raju8830@users.noreply.github.com>
2020-12-30 22:52:49 +0600

    e-commerce-1
    e-commerce-1

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+/* Side Bart Menu*/
+function openmenu(){
+	document.getElementById('side-menu').style.display="block";
+	document.getElementById('menu-btn').style.display="none";
+	document.getElementById('clos-btn').style.display="block";
+}
+function closemenu(){
+	document.getElementById('side-menu').style.display="none";
+	document.getElementById('menu-btn').style.display="block";
+	document.getElementById('clos-btn').style.display="none";
+}
